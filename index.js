8564ca23de26c37bd65f82b1faa826a7e968d51b
Aisha <husseinaisha256@gmail.com>
2026-01-11 19:32:39 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
+
+let random = Math.floor(Math.random() * 1000) + 1; // Random 1-1000
+
+let num3 = 44;
+let num4 = 10;
+let mod = num3 % num4; // 44 % 10 = 4
+
+let numbers = [10, 5, 20, 15, 8];
+let max = Math.max(...numbers); // Using spread operator
 // Write your code here
